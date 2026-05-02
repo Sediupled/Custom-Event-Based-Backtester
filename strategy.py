0d46b715f3219be1a7b4ec58e2489e2a9a49c9c4
Chaitanya <chaitanya.rook@gmail.com>
2026-05-02 18:49:35 +0530

    Added Bare minimum Strategy and Portfolio classes

---
+++ strategy.py
@@ -0,0 +1,43 @@
+import datetime
+import numpy as np
+import pandas as np
+import Queue
+
+from abc import ABCMeta, abstractmethod
+from event import SignalEvent
+
+class Strategy(metaclass=ABCMeta):
+
+
+    @abstractmethod
+    def calculate_signals(self):
+        raise NotImplementedError("Calculate signal unimplemented");
+
+
+
+class BuyAndHoldStrategy(Strategy):
+
+    def __init__(self, bars, events):
+        self.bars = bars
+        self.symbol_list = self.bars.ticker_list
+        self.events = events
+
+
+        self.bought = self._calculate_initial_bought()
+
+
+    def _calculate_initial_bought(self):
+        bought = {}
+        for s in self.symbol_list:
+            bought[s] = False
+        return bought
+
+    def calculate_signals(self, event):
+        if event.type == 'MARKET':
+            for s in self.symbol_list:
+                bars = self.bars.get_latest_bars(s, N=1)
+                if bars is not None and bars != []:
+                    if self.bought[s] == False:
+                        signal = SignalEvent(bars[0][0], bars[0][1], 'LONG')
+                        self.events.put(signal)
+                        self.bought[s] = True
