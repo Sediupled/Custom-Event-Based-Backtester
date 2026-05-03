4629a3cb71611ab7cfb285eafc5febadff760275
Chaitanya <chaitanya.rook@gmail.com>
2026-05-03 14:42:11 +0530

    Basic Execution added, naive implementation finished

---
+++ execution.py
@@ -0,0 +1,22 @@
+import datetime
+import Queue
+
+from abc import ABCMeta, abstractmethod
+
+from event import FillEvent, OrderEvent
+
+class ExecutionHandler(metaclass=ABCMeta):
+
+    @abstractmethod
+    def execute_order(self,event):
+        raise NotImplementedError("execute order not made")
+
+
+class SimulatedExecutionHandler(ExecutionHandler):
+    def __init__(self, events):
+        self.events = events
+
+    def execute_order(self, event):
+        if event.type == 'ORDER':
+            fill_event = FillEvent(datetime.datetime.utcnow(), event.symbol, 'ARCA', event.quantity, event.direction, None)
+            self.events.put(fill_event)
